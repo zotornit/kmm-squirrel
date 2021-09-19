cd523f4b8594f722c14a5f2051705f99b8aaec2d
Thomas Pronold <tp@zotorn.de>
2021-09-19 20:16:42 +0200

    [TASK] Use latest Android SDK 31

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,11 +48,11 @@ kotlin {
 }
 
 android {
-    compileSdkVersion(29)
+    compileSdkVersion(31)
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     defaultConfig {
         minSdkVersion(24)
-        targetSdkVersion(29)
+        targetSdkVersion(31)
     }
 }
 
