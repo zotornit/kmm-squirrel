72c55afdc67e6cbb2d750cf216f00d978e5b8349
Thomas Pronold <tp@zotorn.de>
2021-09-20 10:32:31 +0200

    [FIX] Gradle Plugin version should match Gradle Wrapper version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,8 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.namespace == "com.android" || requested.id.name == "kotlin-android-extensions") {
-                useModule("com.android.tools.build:gradle:4.0.2")
+                // version should kinda match gradle properties version, current values at :https://developer.android.com/studio/releases/gradle-plugin
+                useModule("com.android.tools.build:gradle:7.0.2")
             }
         }
     }
