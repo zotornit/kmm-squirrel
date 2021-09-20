4abf7445844db0667f0ed82e7e523fdde3c62fbe
Thomas Pronold <tp@zotorn.de>
2021-09-20 13:29:33 +0200

    [TASK] comment test files

--- src/androidTest/kotlin/de/zotorn/kmm_squirrel/androidTest.kt
+++ src/androidTest/kotlin/de/zotorn/kmm_squirrel/androidTest.kt
@@ -1,12 +1,5 @@
-//package de.zotorn.kmm_squirrel
-//
-//import org.junit.Assert.assertTrue
-//import org.junit.Test
-//
-//class AndroidGreetingTest {
-//
-////    @Test
-////    fun testExample() {
-////        assertTrue("Check Android is mentioned", Greeting().greeting().contains("Android"))
-////    }
-//}
+package de.zotorn.kmm_squirrel
+
+/*
+    No tests yet
+ */
--- src/iosTest/kotlin/de/zotorn/kmm_squirrel/iosTest.kt
+++ src/iosTest/kotlin/de/zotorn/kmm_squirrel/iosTest.kt
@@ -1,12 +1,5 @@
-//package de.zotorn.kmm_squirrel
-//
-//import kotlin.test.Test
-//import kotlin.test.assertTrue
-//
-//class IosGreetingTest {
-//
-////    @Test
-////    fun testExample() {
-////        assertTrue(Greeting().greeting().contains("iOS"), "Check iOS is mentioned")
-////    }
-//}
+package de.zotorn.kmm_squirrel
+
+/*
+    No tests yet
+ */
